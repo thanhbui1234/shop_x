14a3e237900be709feae5fa5dbbfaff00bd94369
thanh <miktenthanh@gmail.com>
2023-01-07 01:03:01 +0700

    admin

--- include/nav.php
+++ include/nav.php
@@ -9,6 +9,9 @@
          </button>
          <div class="collapse navbar-collapse" id="navbarResponsive">
              <ul class="navbar-nav text-uppercase ms-auto py-4 py-lg-0">
+                 <li class="nav-item">
+                     <a class="nav-link" href="/admin//index.php">Admin</a>
+                 </li>
 
                  <li class="nav-item">
                      <a class="nav-link" href="#portfolio">Sản phẩm</a>
