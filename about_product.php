dd9a87fbd562f9005dfa8eeb8ba265242b64b0ee
thanh <miktenthanh@gmail.com>
2023-01-18 17:35:20 +0700

    cmtt

--- about_product.php
+++ about_product.php
@@ -39,8 +39,56 @@ include './include/header.php'
 
 
 
+    <div id="commnets" class="bg-bg-light container">
+        <?php if (isset($_SESSION['userId'])) {?>
+        <section>
+            <div class="form-floating shadow  bg-body rounded ">
+                <textarea class="form-control" placeholder="Leave a comment here" id="textcmt"></textarea>
+                <label for="floatingTextarea">Comments</label>
+            </div>
 
 
+            <div class="d-flex flex-row justify-content-between mt-3">
+                <div>
+                    <input hidden id="imgCmt" type="file">
+                    <label class="btn btn-primary" for="imgCmt">Thêm ảnh</label>
+                </div>
+
+                <button name="sendCmt" type="submit" class="btn btn-success">Gửi</button>
+
+
+            </div>
+        </section>
+        <?php } else {?>
+
+        <a href="">Đăng nhập để bình luận</a>
+
+        <?php }?>
+
+
+
+    </div>
+
+
+    <section id="cmtUsers" class="">
+
+        <hr>
+
+        <img width="50" src="/uploads//11.jpg" alt=""> <span>Bui Chi Thanh</span>
+
+        <p>2023-01-09 14:56 </p>
+
+        <p>Angelababy xác nhận yêu đương với bạn trai cũ Đàm Tùng Vận: Đẹp đôi đấy nhưng có gì đó sai sai
+            Anh Tú</p>
+
+
+        <img width="35" src="/uploads//111.jpg" alt="">
+
+        <hr>
+
+
+
+    </section>
 
 
     <?php include './include/footer.php'?>
